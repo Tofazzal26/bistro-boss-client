6ad18656e865fdea8891fa61ef0c607c29e1aa5b
Tofazzal26 <afranislamabir6789@gmail.com>
2024-05-16 16:25:51 +0600

    navbar and banner                                              style finish

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../../Shared/Footer/Footer";
+import NavBar from "../../Shared/NavBar/NavBar";
 
 const Root = () => {
   return (
     <div>
+      <NavBar />
       <Outlet />
       <Footer />
     </div>
